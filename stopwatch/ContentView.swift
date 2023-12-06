40cb0e0ef03910f1a97eaca182ba25436fc38217
mohd-kasif <kashifmd123@gmail.com>
2023-12-06 11:42:47 +0530

    Initial Commit

---
+++ stopwatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stopwatch
+//
+//  Created by Apple on 06/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stopwatch/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch
+//
+//  Created by Apple on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
